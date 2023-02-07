aa6401805db7060562a13e87305c22ac8b32d824
Anton Gridushko <anton.gridushko@gmail.com>
2023-02-07 22:38:57 +0300

    Add some aspects for a deeper understanding.

---
+++ Rabbit.Consumer/Receiver.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Console = System.Console;
+
+namespace Rabbit.Consumer;
+
+public class Receiver
+{
+	public static void Main(string[] args)
+	{
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using var connection = factory.CreateConnection();
+		using var channel = connection.CreateModel();
+		channel
+			.QueueDeclare("queueName", false, false, false, null);
+
+		var consumer = new EventingBasicConsumer(channel);
+		consumer.Received += (model, basicDeliverEventArgs) =>
+		{
+			var body = basicDeliverEventArgs.Body.ToArray();
+			var message = Encoding.UTF8.GetString(body);
+			Console.WriteLine("Received Message is: {0}...", message);
+		};
+		channel.BasicConsume("queueName", true, consumer);
+		Console.ReadLine();
+	}
+}
--- Rabbit.Producer/Sender.cs
+++ Rabbit.Producer/Sender.cs
@@ -1,4 +1,6 @@
-﻿using Console = System.Console;
+﻿using RabbitMQ.Client;
+using System.Text;
+using Console = System.Console;
 
 namespace Rabbit.Producer;
 
@@ -6,6 +8,27 @@ public class Sender
 {
 	public static void Main(string[] args)
 	{
-		Console.WriteLine();
+		var factory = new ConnectionFactory() { HostName = "localhost" };
+
+		using (var connection = factory.CreateConnection())
+
+		//1 - channel:
+		using (var channel = connection.CreateModel())
+		{
+			//2 - queue:
+			channel.QueueDeclare("queueName", false, false, false, null);
+
+			for (var i = 0; i < 30; i++)
+			{
+				//3 - message:
+				var message = $"RabbitMQ message {i}";
+				var body = Encoding.UTF8.GetBytes(message);
+
+				//4 - publish body with message in channel:
+				channel.BasicPublish("", "queueName", null, body);
+				Console.WriteLine($"Message number {i} was successfully sent.");
+			}
+		}
+		Console.ReadLine();
 	}
 }
