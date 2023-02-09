096c29c1e78f11733530ce17b8ce2f96096ed572
Anton Gridushko <anton.gridushko@gmail.com>
2023-02-09 23:32:00 +0300

    Add in container objects for future injections.

---
+++ Infrastructure.Ioc/Rabbit.Infrastructure.Ioc/DependencyContainer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Rabbit.Domain.Core.Bus;
+using Rabbit.Infrastructure.Bus;
+
+namespace Rabbit.Infrastructure.Ioc;
+
+public class DependencyContainer
+{
+	public static void RegisterServices(IServiceCollection services)
+	{
+		//Domain Bus
+		services.AddTransient<IEventBus, RabbitMqBus>();
+	}
+}
